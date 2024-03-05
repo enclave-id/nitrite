77e4febc4c0ddf0e07314eb55e77418008e12ed4
ma9o <magogagiovanni@gmail.com>
2024-03-05 10:18:17 +0100

    Graceful error handling. Debug flag

--- cmd/nitrite/main.go
+++ cmd/nitrite/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"encoding/base64"
 	"encoding/json"
-	"fmt"
-	"os"
 	"syscall/js"
 	"time"
 
@@ -18,35 +16,66 @@ func main() {
 
 func validateAttestation(this js.Value, args []js.Value) interface{} {
 	fDocument := args[0].String()
+	debug := false
 
-	document, err := base64.StdEncoding.DecodeString(fDocument)
-	if nil != err {
-		fmt.Printf("Provided attestation document is not encoded as a valid standard Base64 string\n")
-		os.Exit(2)
+	if len(args) > 1 && args[1].Type() == js.TypeBoolean {
+		debug = args[1].Bool()
 	}
 
-	res, err := nitrite.Verify(
+	currentTime := time.Now()
+	if debug {
+		currentTime = time.Date(2024, time.March, 3, 16, 0, 0, 0, time.UTC)
+	}
+
+	type response struct {
+		Result string `json:"result"`
+		Err    string `json:"error"`
+	}
+
+	document, err := base64.StdEncoding.DecodeString(fDocument)
+	if err != nil {
+		errorResponse := response{
+			Result: "",
+			Err:    "Provided attestation document is not encoded as a valid standard Base64 string",
+		}
+		errorMessage, _ := json.Marshal(errorResponse)
+		return js.ValueOf(string(errorMessage))
+	}
+
+	res, verifyErr := nitrite.Verify(
 		document,
 		nitrite.VerifyOptions{
-			CurrentTime: time.Now(),
+			CurrentTime: currentTime,
 		},
 	)
 
 	resJSON := ""
-
-	if nil != res {
-		enc, err := json.Marshal(res.Document)
-		if nil != err {
-			panic(err)
+	if res != nil {
+		enc, marshalErr := json.Marshal(res.Document)
+		if marshalErr != nil {
+			errorResponse := response{
+				Result: "",
+				Err:    "Error marshalling the verification result: " + marshalErr.Error(),
+			}
+			errorMessage, _ := json.Marshal(errorResponse)
+			return js.ValueOf(string(errorMessage))
 		}
-
 		resJSON = string(enc)
 	}
 
-	if nil != err {
-		fmt.Printf("Attestation verification failed with error %v\n", err)
-		os.Exit(2)
+	if verifyErr != nil {
+		errorResponse := response{
+			Result: "",
+			Err:    "Attestation verification failed with error: " + verifyErr.Error(),
+		}
+		errorMessage, _ := json.Marshal(errorResponse)
+		return js.ValueOf(string(errorMessage))
 	}
 
-	return js.ValueOf(resJSON)
+	successResponse := response{
+		Result: resJSON,
+		Err:    "",
+	}
+	finalMessage, _ := json.Marshal(successResponse)
+	return js.ValueOf(string(finalMessage))
 }
